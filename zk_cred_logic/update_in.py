c5ad7afada88e529787e498408b50973614bc883
sashafrolov <sasha07974@gmail.com>
2025-02-17 20:05:29 -0500

    Remove unused imports and unused function from update_in.py

--- zk_cred_logic/update_in.py
+++ zk_cred_logic/update_in.py
@@ -1,34 +1,12 @@
 import json
 import os
 import random
-# import hashlib
-# from ecdsa import SigningKey, SECP256k1
-# from ecdsa.util import sigencode_string
 
 if not os.path.exists("input.json") or os.path.getsize("input.json") == 0:
     old_in = None
 else:
     with open("input.json", "r") as in_json:
         old_in = json.load(in_json)
-
-def update_ip_strings(old_ips, new_ip, ip_exists):
-	num_ips = len(old_ips)
-	new_ips = old_ips.copy()
-	
-	if ip_exists:
-		# Move existing IPs up
-		for i in range(num_ips - 1, 0, -1):
-			new_ips[i] = new_ips[i-1]
-		# Add new IP at the end
-		new_ips[num_ips - 1] = new_ip
-	else:
-		# Add new IP to the first available slot
-		for i in range(num_ips):
-			if new_ips[i] == "0" * 39:
-				new_ips[i] = new_ip
-				break
-	
-	return new_ips
 
 def format_and_pad_ip(ip):
 	# Pad IPv4 to 15 characters (xxx.xxx.xxx.xxx)
